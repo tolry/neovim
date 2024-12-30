11e3152f91b0888f1005a60631035f2817cf0edf
Tobias Olry <tobias@tideways-gmbh.com>
2024-12-30 21:44:11 +0100

    most common php indentation settings

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,19 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- PHP indentation settings
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("php_indent"),
+  pattern = "php",
+  callback = function()
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.expandtab = true
+  end,
+})
